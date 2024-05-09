3775a45a554a7920a60e9869406079998b24790b
younga298 <156628700+younga298@users.noreply.github.com>
2024-05-09 09:25:19 +0900

    0509

--- javascript/last.js
+++ javascript/last.js
@@ -1,5 +1,14 @@
-$(function(){   
-    $('.text-middle li:last').click(function(){   
-        window.alert('감사합니다.');
-    })
-  });
+const modal = document.querySelector(".middle-popup");
+const openBtn = document.querySelector(".popup_show"); // 괜찮아요 버튼
+const closeBtn = document.querySelector('.close-button'); // 화살표 버튼
+const text = document.querySelector(".text-top");
+const text1 = document.querySelector(".text-middle");
+
+const toggleModal = () => {
+    modal.style.display = (modal.style.display === "block") ? "none" : "block";
+    text.style.display = (text.style.display === "none") ? "block" : "none";
+    text1.style.display = (text1.style.display === "none") ? "block" : "none";
+  };
+
+openBtn.addEventListener("click", toggleModal);
+closeBtn.addEventListener("click", toggleModal);
